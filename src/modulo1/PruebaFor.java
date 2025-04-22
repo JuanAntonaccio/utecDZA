5f64cf85f3a36285f66515da73aa68b10c8eaa6c
Juan <jmantonaccio@gmail.com>
2025-04-22 11:37:26 -0300

    Mi primer commit

---
+++ src/modulo1/PruebaFor.java
@@ -0,0 +1,12 @@
+package modulo1;
+
+public class PruebaFor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("i vale: " + i);
+
+        }
+        System.out.println("Se termino el for");
+    }
+
+}
