2ac04a0964527b0ba61b2000d4edf988703b033a
Juan <jmantonaccio@gmail.com>
2025-04-22 12:02:25 -0300

    Segundo commit

---
+++ src/modulo1/PruebaFor2.java
@@ -0,0 +1,25 @@
+package modulo1;
+
+public class PruebaFor2 {
+    public static void main(String[] args) {
+        // variable acumulador
+        int suma = 0;
+        // variable contador
+        int contador=0;
+        for (int i = 1; i <=100 ; i++) {
+            if (i % 2 == 0) {
+                suma += i;
+                contador++;
+            }
+            //suma=suma+i;
+        }
+        System.out.println("La suma de los pares es: "+suma);
+        System.out.println("La cantidad de numeros pares es: "+contador);
+        System.out.println();
+        suma=0;
+        for (int i = 2; i <=100 ; i=i+2) {
+            suma += i;
+        }
+        System.out.println("segunda forma - La suma de los pares es: "+suma);
+    }
+}
---
+++ src/modulo1/PruebaWhile.java
@@ -0,0 +1,11 @@
+package modulo1;
+
+public class PruebaWhile {
+    public static void main(String[] args) {
+        int indice=0;
+        while(indice < 5){
+            System.out.println("El indice vale: "+indice);
+            indice++;
+        }
+    }
+}
